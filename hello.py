02017da55f6477b6e70892d51a3838b4830a633d
derek <dmonarch134@gmail.com>
2022-05-17 23:06:53 -0500

    flask route set up and added base html

--- hello.py
+++ hello.py
@@ -1,10 +1,11 @@
-from flask import Flask, jsonify, request
+from flask import Flask, redirect, url_for, request, render_template
 
 app= Flask(__name__)
 
 @app.route("/")
-def hello():
-    return jsonify({"about": "Hello World!"})
+def home():
+    return render_template('index.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
