f677ff66a465a671e8ba7d1fffed9d55da6ce9dd
derek <dmonarch134@gmail.com>
2022-05-17 23:17:57 -0500

    man running broke when added actual.py to routes

---
+++ actual.py
@@ -0,0 +1,16 @@
+#python code that will take in job title and return a message saying they can get the job
+what = input("What is your job title? ")
+print ("You can be a " + what + "!")
+
+if what == "programmer":
+    print("You can get the job! you just need to be good at learning new things!")
+elif what == "teacher":
+    print("You can get the job, but you need to be good at teaching")
+elif what == "doctor":
+    print("You can get the job, but you need to be good at treating people")
+elif what == "lawyer":
+    print("You can get the job, but you need to be good at defending people")
+elif what == "engineer":
+    print("You can get the job, but you need to be good at fixing things")
+elif what == "scientist":
+    print("You can get the job, but you need to be good at analyzing things")
--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
 from flask import Flask, redirect, url_for, request, render_template
+from actual import *
 
 app= Flask(__name__)
 
@@ -6,6 +7,9 @@ app= Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route("/jobs")
+def jobs():
+    return render_template('actual.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
