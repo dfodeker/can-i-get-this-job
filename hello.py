c1c80fec792758190307cf68f0f79bcd9ecf4422
derek <dmonarch134@gmail.com>
2022-05-17 20:48:25 -0500

    started flask app with api

--- hello.py
+++ hello.py
@@ -1,6 +1,10 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, jsonify, request
+
+app= Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return jsonify({"about": "Hello World!"})
+
+if __name__ == "__main__":
+    app.run(debug=True)
