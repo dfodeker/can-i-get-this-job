c560fc439c97e3c9c5eebc196686f559e42218bf
derek <dmonarch134@gmail.com>
2022-05-18 00:19:03 -0500

    changed users to resutls

--- hello.py
+++ hello.py
@@ -1,10 +1,11 @@
+from turtle import title
 from flask import Flask, redirect, url_for, request, render_template, flash
 from actual import *
 
 app= Flask(__name__)
 app.config['SECRET_KEY'] = 'a88a26f4a54e74b3054b40f709f5781aadbed9558c941576'
 
-jobs = [{'Job_ID': 1, 'Job_Title': 'Software Engineer'}]
+results = [{'Job_Title': 'Software Engineer'}]
 
 @app.route("/")
 def home():
@@ -13,13 +14,14 @@ def home():
 @app.route("/jobs", methods=["GET", "POST"])
 def jobs():
     if request.method == "POST":
-        user = request.form['nm']
-        return redirect(url_for("user", usr=user))
+        results = request.form['nm']
+        return redirect(url_for("results", res=results))
     else:
         return render_template('actual.html')
-@app.route("/user/<usr>")
-def user(usr):
-    return f"<h1>Hello {usr}</h1>"
-
+@app.route("/results/<res>")
+def results(res):
+    if request.method == "POST":
+        results = request.form['jobsT']
+        return redirect(url_for("results", results=res))
 if __name__ == "__main__":
     app.run(debug=True)
